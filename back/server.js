9d48cdca1d98eed23bf4d684675b54b4225d0f39
Andresdvp <96126942+Andresdvp@users.noreply.github.com>
2023-11-07 09:41:54 -0500

    clase 3

--- back/server.js
+++ back/server.js
@@ -1,1 +1,10 @@
-const app = require("app");
+const app = require("./app");
+//setear el archivo de configuracion
+const dotenv = require("dotenv");
+dotenv.config({path:'back/config/config.env'});
+
+//escucha el puerto
+const server = app.listen(process.env.PORT, ()=>{
+    //regresa que si estoy conectado al servidor
+    console.log(`Servidor iniciado en el puerto:  ${process.env.PORT}  en modo:  ${process.env.NODE_ENV}`);
+}); 
