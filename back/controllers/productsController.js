64d3edf2dde269d3c531d68ec35cd83793a4d3b7
Andresdvp <96126942+Andresdvp@users.noreply.github.com>
2024-03-31 11:06:08 -0500

    Clase 19B. Clase Extra 1. Controladores y Rutas para Reviews de nuestro proyecto C_19B

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -2,7 +2,7 @@
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const producto = require("../models/productos");
 const ErrorHandler = require("../utils/errorHandler");
-const fetch = (url)=> import ('node-fetch').then(({default:fetch})=>fetch(url))//usurpacion del import
+const fetch = (url) => import('node-fetch').then(({ default: fetch }) => fetch(url))//usurpacion del import
 
 
 
@@ -11,12 +11,12 @@ const fetch = (url)=> import ('node-fetch').then(({default:fetch})=>fetch(url))/
 
 //ver lista productos
 //req = requiere, res = respuesta, next = siguiente
-exports.getProducts = catchAsyncErrors( async (req, res, next) => {
+exports.getProducts = catchAsyncErrors(async (req, res, next) => {
     const productos = await producto.find();
     //if para el error
     if (!productos) {
         return next(new ErrorHandler("Informacion no encontrada", 404))
-        
+
     }
     res.status(200).json({
         sucess: true,
@@ -28,27 +28,27 @@ exports.getProducts = catchAsyncErrors( async (req, res, next) => {
 
 
 //ver producto por  id
-exports.getProductsById = catchAsyncErrors( async (req, res, next) => {
+exports.getProductsById = catchAsyncErrors(async (req, res, next) => {
     const product = await producto.findById(req.params.id);//variable de tipo modificable
     if (!product) {
         return next(new ErrorHandler("Producto no encontrado", 404))
-        
+
     }
 
     res.status(200).json({
-        sucess: true, 
+        sucess: true,
         message: "Aqui debajo encuentras informacion de tu producto: ",
         product
     })
 })
 
 //Update de un producto
-exports.updateProduct =catchAsyncErrors( async (req, res, next) => {
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
     //buscar por id y mostrar mensaje al usuario de que no se encontro el producto
     let product = await producto.findById(req.params.id);
     if (!product) {
         return next(new ErrorHandler("Producto no encontrado", 404))
-        
+
     }
 
     //buscar por id y actualizar
@@ -65,12 +65,12 @@ exports.updateProduct =catchAsyncErrors( async (req, res, next) => {
 })
 
 //Delete de un producto
-exports.deleteProduct =catchAsyncErrors( async (req, res, next) => {
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
     //buscar por id y mostrar mensaje al usuario de que no se encontro el producto
-    const product = await producto.findById(req.params.id); 
+    const product = await producto.findById(req.params.id);
     if (!product) {
         return next(new ErrorHandler("Producto no encontrado", 404))
-        
+
     }
 
     //Eliminar el producto
@@ -78,46 +78,135 @@ exports.deleteProduct =catchAsyncErrors( async (req, res, next) => {
     await product.deleteOne();
 
     res.status(200).json({
-        sucess:true,
-        message:"Producto eliminado exitosamente" 
+        sucess: true,
+        message: "Producto eliminado exitosamente"
     })
 
-}  )
+})
 
 
 
 //crear nuevo producto ver en /api/productos
 
-exports.newProduct =  catchAsyncErrors( async (req, res, next) => {
-    req.body.user= req.user.id;
+exports.newProduct = catchAsyncErrors(async (req, res, next) => {
+    req.body.user = req.user.id;
     const product = await producto.create(req.body);
-    
+
 
     //respuesta 201 y lleve el producto con los datos de product
     res.status(201).json({
         sucess: true,
         product
     })
-} )
+})
+
+//Reviews
+
+//crear o actualizar una review
+exports.createProductReview = catchAsyncErrors(async (req, res, next) => {
+    const { rating, comentario, idProducto } = req.body;
+
+    const opinion = {
+        nombreCliente: req.user.nombre,
+        rating: Number(rating),
+        comentario
+    }
+    const product = await producto.findById(idProducto);
+
+    //sacar el nombre del cliente
+    const isReviewed = product.opiniones.find(item =>
+        item.nombreCliente === req.user.nombre)
+
+    //verificar si mi comentario exite para actualizarlo        
+    if (isReviewed) {
+        product.opiniones.forEach(opinion => {
+            if (opinion.nombreCliente === req.user.nombre) {
+                opinion.comentario = comentario,
+                    opinion.rating = rating
+            }
+
+        })
+    } else {
+        product.opiniones.push(opinion)
+        product.numCalificaciones = product.opiniones.length
+    }
+
+    //promedio de las calificacion
+    product.calificacion = product.opiniones.reduce((acc, opinion) =>
+        opinion.rating + acc, 0) / product.opiniones.length
+
+    await product.save({ validateBeforeSave: false });
+
+    res.status(200).json({
+        sucess: true,
+        message: "Agradecemos tus comentarios"
+    })
+
+})
+
+
+//ver todas las reviews de un producto
+exports.getProductsReviews = catchAsyncErrors(async (req, res, next) => {
+    const product = await producto.findById(req.query.id);
+
+    res.status(200).json({
+        sucess: true,
+        opiniones: product.opiniones
+    })
+})
+
+//Eliminar review
+exports.deleteReview = catchAsyncErrors(async (req, res, next) => {
+    const product = await producto.findById(req.query.idProducto);
+
+    const opiniones = product.opiniones.filter(opinion =>
+        opinion._id.toString() != req.query.idReview.toString());
+
+    const numCalificaciones = opiniones.length;
+
+    const calificacion = product.opiniones.reduce((acc, Opinion) =>
+        Opinion.rating + acc, 0) / opiniones.length
+
+
+
+
+    await producto.findByIdAndUpdate(req.query.idProducto, {
+        opiniones,
+        calificacion,
+        numCalificaciones
+    }, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+
+    res.status(200).json({
+        sucess: true,
+        message: "Review se elimino correctamente"
+
+    })
+})
+
+
 
 
 //fetch
 //ver todos los productos 
-function verProductos(){
+function verProductos() {
     fetch('http://localhost:4000/api/productos')
-    .then(res=>res.json())
-    .then(res=>console.log(res))
-    .catch(err=>console.error(err))
+        .then(res => res.json())
+        .then(res => console.log(res))
+        .catch(err => console.error(err))
 }
 //verProductos();
- 
-//ver por id
 
-function verProductosPorID(id){
-    fetch('http://localhost:4000/api/producto/'+id)
-    .then(res=>res.json())
-    .then(res=>console.log(res))
-    .catch(err=>console.error(err))
+//ver producto por id
+
+function verProductosPorID(id) {
+    fetch('http://localhost:4000/api/producto/' + id)
+        .then(res => res.json())
+        .then(res => console.log(res))
+        .catch(err => console.error(err))
 
 }
 
--- back/routes/products.js
+++ back/routes/products.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-const {getProducts, newProduct, getProductsById, updateProduct, deleteProduct}=require("../controllers/productsController");
-const { isAuthenticatedUser,authorizeRole } = require("../middleware/auth");
+const { getProducts,
+    newProduct,
+    getProductsById,
+    updateProduct,
+    deleteProduct,
+    createProductReview, 
+    getProductsReviews,
+    deleteReview} = require("../controllers/productsController");
+const { isAuthenticatedUser, authorizeRole } = require("../middleware/auth");
 //rutas de productos 
 //Probar autenticacion
-router.route('/productos').get( getProducts);//establecer desde que ruta queremos ver el getProduct
-router.route('/producto/nuevo').post(isAuthenticatedUser,authorizeRole("admin"),newProduct);// ruta del nuevo producto
+router.route('/productos').get(getProducts);//establecer desde que ruta queremos ver el getProduct
+router.route('/producto/nuevo').post(isAuthenticatedUser, authorizeRole("admin"), newProduct);// ruta del nuevo producto
 router.route('/producto/:id').get(getProductsById);//ver producto por id 
-router.route('/producto/:id').put(isAuthenticatedUser,authorizeRole("admin"),updateProduct);//Actualizar el producto
-router.route('/producto/:id').delete(isAuthenticatedUser,authorizeRole("admin"),deleteProduct);//eliminar producto por id
+router.route('/producto/:id').put(isAuthenticatedUser, authorizeRole("admin"), updateProduct);//Actualizar el producto
+router.route('/producto/:id').delete(isAuthenticatedUser, authorizeRole("admin"), deleteProduct);//eliminar producto por id
 
+//comentarios
+router.route("/review").put(isAuthenticatedUser, createProductReview)
+router.route("/reviews").get(getProductsReviews)
+router.route("/review").delete(isAuthenticatedUser,deleteReview)
 
-
-
-
-module.exports=router; 
+module.exports = router; 
